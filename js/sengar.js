fa779c295ef194d7a6f48824409c98d18e170f0e
Vishal Sengar <vishal_sengar@yahoo.com>
2018-10-02 00:01:23 +0530

    Update sengar.js

--- js/sengar.js
+++ js/sengar.js
@@ -55,12 +55,12 @@ var projects = {
 		'key': 'products'
 	},
 	
-	'circleplanner': {
-		'title': 'Circle Planner',
-		'description': 'A graphical way to represent and plan your whole month on a single spread of paper. The reminders and other schedules can be kept in 12 sheets of any kind of notebook or a seperate notebook',
-		'color': '#FFF',
-		'key': 'graphics'
-	},
+	//'circleplanner': {
+	//	'title': 'Circle Planner',
+	//	'description': 'A graphical way to represent and plan your whole month on a single spread of paper. The reminders and other schedules can be kept in 12 sheets of any kind of notebook or a seperate notebook',
+	/	'color': '#FFF',
+	//	'key': 'graphics'
+	//},
 
 	'nest': {
 		'title': 'Nest',
